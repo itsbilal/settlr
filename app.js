7d8a7c403fcc88775537bb255a65f037391b62b5
Tim Mui <timmui123@gmail.com>
2016-02-20 15:37:51 -0500

    Added user registration endpoint
    Import Postman Collection to try it out.

--- app.js
+++ app.js
@@ -30,9 +30,11 @@ mongoose.connection.once('open', function(){
 
   var routes = require('./routes/index');
   var users = require('./routes/users');
+  var api = require('./routes/api');
 
   app.use('/', routes);
   app.use('/users', users);
+  app.use('/api', api);
 
   // catch 404 and forward to error handler
   app.use(function(req, res, next) {
