605efe7972fbf34cf2fac5dbca907557c6e5d41c
Bilal Akhtar <me@itsbilal.com>
2016-02-20 21:29:56 -0500

    Fix model index.js issues when it tries to load DS_Store

--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ var fs = require("fs");
 
 module.exports = function() {
   fs.readdirSync(__dirname).forEach(function(file) {
-    if (file === "index.js"){
+    var extension = file.substr(file.indexOf('.'));
+    if (file === "index.js" || extension !== ".js"){
       return;
     }
     var name = file.substr(0, file.indexOf('.'));
